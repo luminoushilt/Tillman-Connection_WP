01913ecc59a2478fd46b2f475f8a44a3ba176bc4
Hilton Williams <hwilliams@luminoushilt.com>
2020-02-27 17:14:42 -0800

    Add typography options and fonts.  Created homepage for wordpress site.

--- wp-content/themes/tillman-connection/resources/views/front-page.blade.php
+++ wp-content/themes/tillman-connection/resources/views/front-page.blade.php
@@ -7,57 +7,57 @@
 @section('content')
 <!-- front-page.blade.php -->
 
-<section class="section">
-	<div class="section-hero">
-		<a class="button button-default get-connected scroll-link" href="#greeting">Get Connected</a>
+<section class="hero get-connected-container">
+	@php
+		$featured_image = get_field('banner_image');
+	@endphp
+	<img src="{{ $featured_image['url'] }}" alt="{{ $featured_image['alt'] }}" class="img-responsive">
+	<a class="button is-tillman-teal get-connected scroll-link" href="#greeting">Get Connected</a>
+</section>
+<section class="hero hero-section">
+	<div class="hero-body">
+		<div class="container">
+			<div class="columns">
+				<div class="column is-half-desktop family-greeting">
+					<h1 class="title">
+						{{ the_field('page_intro') }}
+					</h1>
+					{{ the_field('family_greeting') }}
+				</div>
+				<div class="column is-half-desktop tillman-connection-logo-section">
+					<h2>Tillman Connection</h2>
+					<i class="fas fa-wifi tillman-connection"></i>
+					<span>21st Century</span>
+				</div>
+			</div>
+		</div>
 	</div>
 </section>
-<section class="section columns">
-	<h1 class="title">
-		{{ the_field('page_intro') }}
-	</h1>
-	<div class="column is-half family-greeting">
-		{{ the_field('family_greeting') }}
-	</div>
-	<div class="column is-half tillman-connection-logo-section">
-		<h2>Tillman Connection</h2>
-		<i class="fas fa-wifi tillman-connection"></i>
-		<h2>21st Century</h2>
+<section class="hero hero-section">
+	<div class="container hero-section-content">
+		{{ the_field('reunion_mission_statement') }}
 	</div>
 </section>
-<section class="section section-content">
-	<p>
-		Our guiding theme is "Strengthening Family through our connection." As a
-		committee, we are charging family to not only clinch to this theme for
-		the reunion but to honor and practice preserving the Tillman legacy.
-	</p>
+<section class="hero hero-section">
+	<div class="section-break-image">
+		<div class="container">
+			{{ the_field('location_pitch') }}
+		</div>
+	</div>
 </section>
-<section class="section no-margin section-break-image">
-	<p>
-		How will our theme be demonstrated throughout our weekend in the
-		nation's capital and beyond?
-	</p>
+<section class="hero hero-section">
+	<div class="container hero-section-content">
+		{{ the_field('concluding_statement') }}
+	</div>
 </section>
-<section class="section section-content">
-	<p>
-		The 2021 committee has not only committed to planning a funfilled &amp;
-		memorable weekend, but has also committed to bridging the generations
-		within our family together. This theme will be experienced through
-		multiple activities and events. As you continue to explore our family
-		website, you will see a preliminary outline of the weekend itinerary.
-		Please pay close attention to deadlines/important dates. We encourage
-		you to visit our family website often for updates and messages. No
-		exceptions to deadlines! For a successful outcome, we need your
-		unwavering support, continuous participation and honoring of this family
-		legacy.
-	</p>
-</section>
-<section class="section section-content">
-	<p>
-		For any questions, concerns or suggestions feel free to
-		<a class="section-content-link" href="mailto:info@tillmanconnection.com">
-			contact one of your committee members</a>.
-	</p>
+<section class="hero hero-section">
+	<div class="container hero-section-content">
+		<p>
+			For any questions, concerns or suggestions feel free to
+			<a class="section-content-link" href="mailto:info@tillmanconnection.com">
+				contact one of your committee members</a>.
+		</p>
+	</div>
 </section>
 
 @endsection
