a33d6563a21607e3ae53939fdd5b910d349de9bf
Hilton Williams <hwilliams@luminoushilt.com>
2020-03-01 22:04:42 -0800

    Create About Page. Update Navbar.  Add mobile styles.

--- wp-content/themes/tillman-connection/resources/views/partials/header.blade.php
+++ wp-content/themes/tillman-connection/resources/views/partials/header.blade.php
@@ -1,10 +1,19 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
+    <nav class="navbar is-tillman center-navbar-content" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="brand brand-logo" href="{{ home_url('/') }}">
+        <i class="fas fa-wifi tillman-connection"></i>
+        <span class="navbar-company-name">{{ get_bloginfo('name', 'display') }}</span>
+        </a>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          @if (has_nav_menu('primary_navigation'))
+            {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+          @endif
+        </div>
+      </div>
     </nav>
   </div>
 </header>
---
+++ wp-content/themes/tillman-connection/resources/views/template-article.blade.php
@@ -0,0 +1,25 @@
+{{--
+  Template Name: Article Page Template
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+<!-- template-article.blade.php -->
+
+@php
+	$featured_image = get_field('banner_background_image');
+@endphp
+
+<section class="article-title hero article-banner-container" style="background-image:url({{ $featured_image['url'] }})">
+	<h1 class="title is-size-1 is-size-3-touch">
+		{{ the_field('page_title') }}
+	</h1>
+</section>
+<section class="hero hero-section">
+	<div class="container hero-section-content is-size-2 is-size-4-touch">
+		{{ the_field('content_area') }}
+	</div>
+</section>
+
+@endsection
