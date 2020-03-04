81a6eafd3e11e7d12636de440dd753549703e500
Hilton Williams <hwilliams@luminoushilt.com>
2020-03-03 20:50:54 -0800

    Add mobile hamburger button.  Add mobile navbar menu.  Update desktop navbar styles to include hover animation.

--- wp-content/themes/tillman-connection/resources/views/partials/header.blade.php
+++ wp-content/themes/tillman-connection/resources/views/partials/header.blade.php
@@ -3,11 +3,16 @@
     <nav class="navbar is-tillman center-navbar-content" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <a class="brand brand-logo" href="{{ home_url('/') }}">
-        <i class="fas fa-wifi tillman-connection"></i>
-        <span class="navbar-company-name">{{ get_bloginfo('name', 'display') }}</span>
+          <i class="fas fa-wifi tillman-connection"></i>
+          <span class="navbar-company-name">{{ get_bloginfo('name', 'display') }}</span>
+        </a>
+        <a role="button" class="navbar-burger tillman-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
         </a>
       </div>
-      <div class="navbar-menu">
+      <div id="navMenu" class="navbar-menu">
         <div class="navbar-end">
           @if (has_nav_menu('primary_navigation'))
             {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
