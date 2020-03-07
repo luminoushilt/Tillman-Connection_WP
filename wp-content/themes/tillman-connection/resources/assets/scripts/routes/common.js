69a226c3703b1bdfd292cf5ab4897a8539518f96
Hilton Williams <hwilliams@luminoushilt.com>
2020-03-07 15:03:13 -0800

    update contact plugin.  Add Committee template, post type, page, and custom field.

--- wp-content/themes/tillman-connection/resources/assets/scripts/routes/common.js
+++ wp-content/themes/tillman-connection/resources/assets/scripts/routes/common.js
@@ -1,6 +1,13 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+    $('.navbar-burger').on('click', () => toggleMenu());
+
+    const toggleMenu = () => {
+      // Toggle the 'is-active' class on both the 'navbar-burger' and the 'navbar-menu'
+      $('.navbar-burger').toggleClass('is-active');
+      $('.navbar-menu').toggleClass('is-active');
+    }
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
