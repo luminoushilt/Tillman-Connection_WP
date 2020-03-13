3f26e8f4971a9c729f9e58565887fa8b740b7a61
Hilton Williams <hwilliams@luminoushilt.com>
2020-03-13 16:19:30 -0700

    Add back to top autoscroll feature.

--- wp-content/themes/tillman-connection/resources/views/partials/footer.blade.php
+++ wp-content/themes/tillman-connection/resources/views/partials/footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="content-info">
-  &copy; 2019-{{ date('Y') }}
   <div class="container">
+  &copy; 2019-{{ date('Y') }}
     @php dynamic_sidebar('sidebar-footer') @endphp
   </div>
 </footer>
+<div class="back-to-top"><i class="fas fa-angle-up sp-arrow-up"></i></div>
