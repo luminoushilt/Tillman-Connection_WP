8b27939cd1d7e5e8acca41d8adea0f949bca7896
Hilton Williams <hwilliams@luminoushilt.com>
2020-03-28 11:05:29 -0700

    Build newsletter page.

--- wp-content/themes/tillman-connection/resources/views/archive-newsletter.blade.php
+++ wp-content/themes/tillman-connection/resources/views/archive-newsletter.blade.php
@@ -1,13 +1,13 @@
 {{--
-  Archive Name: Fundraiser Archive Page
+  Archive Name: Newsletter Archive Page
 --}}
 
 @extends('layouts.app')
 
 @section('content')
-<!-- archive-fund.blade.php -->	
-	<section class="article-title hero article-banner-container" style="background-image:url(@asset('images/us-supreme-court-building.jpg'))">
-		<h1 class="title is-size-1 is-size-3-touch">Fundraisers</h1>
+<!-- archive-newsletter.blade.php -->	
+	<section class="article-title hero article-banner-container" style="background-image:url(@asset('images/white-house.jpg'))">
+		<h1 class="title is-size-1 is-size-3-touch">Newsletters</h1>
 	</section>
   <section class="hero hero-section">
 		<div class="container hero-section-content is-size-2 is-size-4-touch custom-posts-container">
